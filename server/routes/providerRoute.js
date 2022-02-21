10547b14ed8a1a61df054e2565e829592b558e0c
Windyvinoth13 <vinothv.19@uom.lk>
2022-02-21 16:52:22 +0530

    Provider route changes

--- server/routes/providerRoute.js
+++ server/routes/providerRoute.js
@@ -8,9 +8,9 @@ router.post("/", providerController.post_providerType);
 //update verification details
 router.patch("/updateverification/:id", providerController.update_verification);
 //fetch all providers
-router.get("/fetch_providers/", providerController.fetch_providers);
+router.get("/", providerController.fetch_providers);
 //fetch provider using certain id
-router.get("/fetch_provider/:id", providerController.fetch_provider);
+router.get("/:id", providerController.fetch_provider);
 //disable or enable provider
 router.patch("/disable_provider/:id", providerController.disable_provider);
 
