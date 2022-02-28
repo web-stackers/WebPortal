56f41d56c19788b985cd48414f256d9299c2c7d4
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2022-02-28 18:26:48 +0530

    Commited ThirdParty Card Structure

---
+++ client/src/components/thirdParty/ThirdPartyList.js
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import SecondaryUser from "../../services/SecondaryUser";
+import Sbutton from "../Sbutton";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    height: "200px",
+    backgroundColor: "transparent !important",
+    color: "white !important",
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 10,
+    width: "50%",
+  },
+  content: {
+    flex: "1 0 auto",
+  },
+  cover: {
+    width: "20%",
+  },
+  // btngrp: {
+  //   display: "flex",
+  //   flexDirection: "column",
+  //   justifyContent: "space-around",
+  //   height: "50%",
+  //   alignSelf: "center",
+  // },
+  // btn: {
+  //   width: "200px",
+  // },
+}));
+
+const ThirdPartyList = () => {
+  const [thirdParties, setThirdParties] = useState([]);
+
+  const fetchUsers = () => {
+    SecondaryUser.fetchThirdParty()
+      .then((response) => {
+        setThirdParties(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const classes = useStyles();
+
+  return (
+    <div>
+      {" "}
+      {thirdParties.map((thirdParty) => (
+        <div key={thirdParty._id}>
+          <br />
+          <Card className={classes.root}>
+            <CardMedia
+              className={classes.cover}
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <div className={classes.details}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {thirdParty.name.fName} {thirdParty.name.lName}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  <b>Contact Number :</b> {thirdParty.contact.mobile}
+                  <br />
+                  <b>Email :</b> {thirdParty.contact.email}
+                  <br />
+                  <b>Address : </b>
+                  {thirdParty.address}
+                  <br />
+                  <b>Verification Document Type:</b> {thirdParty.verifyDocType}
+                </Typography>
+              </CardContent>
+            </div>
+
+            <CardActions>
+              <Sbutton text="Edit"></Sbutton>
+              <Sbutton text="Disable"></Sbutton>
+            </CardActions>
+          </Card>
+          <br />
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ThirdPartyList;
--- client/src/pages/thirdParty/ThirdParty.js
+++ client/src/pages/thirdParty/ThirdParty.js
@@ -1,12 +1,6 @@
 import Sbutton from "../../components/Sbutton";
 import { useNavigate } from "react-router-dom";
-import SecondaryUser from "../../services/SecondaryUser";
-import { useEffect, useState } from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
+import ThirdPartyList from "../../components/thirdParty/ThirdPartyList";
 
 const ThirdParty = () => {
   const navigate = useNavigate();
@@ -15,60 +9,12 @@ const ThirdParty = () => {
     navigate(path);
   };
 
-  const [thirdParties, setThirdParties] = useState([]);
-
-  const fetchUsers = () => {
-    SecondaryUser.fetchThirdParty()
-      .then((response) => {
-        setThirdParties(response.data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
   return (
     <div>
       <div>
         <Sbutton text="Add New" onClick={routeChange}></Sbutton>
         <br /> <br />
-        {thirdParties.map((thirdParty) => (
-          <div key={thirdParty._id}>
-            <br />
-            <Card sx={{ maxWidth: 445 }} variant="outlined">
-              <CardMedia
-                component="img"
-                height="140"
-                image="/static/images/cards/contemplative-reptile.jpg"
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {thirdParty.name.fName} {thirdParty.name.lName}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {thirdParty.contact.mobile}
-                  <br />
-                  {thirdParty.contact.email}
-                  <br />
-                  {thirdParty.address}
-                  <br />
-                  {thirdParty.verifyDocType}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Sbutton text="Edit" onClick={routeChange}></Sbutton>
-                <Sbutton text="Disable" onClick={routeChange}></Sbutton>
-              </CardActions>
-            </Card>
-            <br />
-            <hr />
-          </div>
-        ))}
+        <ThirdPartyList></ThirdPartyList>
       </div>
     </div>
   );
