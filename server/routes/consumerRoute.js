c128cfae87655cb64449cd1d0cf102e1841dd7c4
kathu@08 <sivalingamk.19@uom.lk>
2022-03-02 12:27:43 +0530

    added comments

--- server/routes/consumerRoute.js
+++ server/routes/consumerRoute.js
@@ -3,9 +3,16 @@ const router = express.Router();
 
 const consumerController  = require('../controllers/consumerController');
 
+// Fetch all consumers
 router.get('/', consumerController.fetch_consumers);
+
+// Fetch consumer by id
 router.get('/:id', consumerController.fetch_consumer);
+
+// Add new consumer to the database
 router.post('/', consumerController.post_consumer);
+
+// Disable or Enable consumer
 router.patch('/:id', consumerController.disable_consumer);
 
 module.exports = router;
