ffb3f155d8f002588ecab6ec9f1f368adcc48c04
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2022-03-07 07:53:53 +0530

    preview value

--- client/src/pages/thirdParty/ThirdPartyProfile.js
+++ client/src/pages/thirdParty/ThirdPartyProfile.js
@@ -3,6 +3,11 @@ import Typography from "@mui/material/Typography";
 import StextField from "../../components/formComponents/StextField";
 import Sbutton from "../../components/Sbutton";
 import SecondaryUser from "../../services/SecondaryUser";
+import { useState } from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const ThirdPartyProfile = () => {
   const location = useLocation();
@@ -10,18 +15,22 @@ const ThirdPartyProfile = () => {
   const ID = location.state._id;
   const fName = location.state.name.fName;
   const lName = location.state.name.lName;
+  const email = location.state.contact.email;
+  const mobile = location.state.contact.mobile;
   const address = location.state.address;
+  const verifyDocType = location.state.verifyDocType;
 
-  // const [inputs, setInputs] = useState({});
+  const [inputs, setInputs] = useState({});
 
-  // const handleChange = (event) => {
-  //   const name = event.target.name;
-  //   const value = event.target.value;
-  //   setInputs((values) => ({ ...values, [name]: value }));
-  // };
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
+    console.log(inputs);
     // SecondaryUser.addNew(inputs);
   };
 
@@ -33,11 +42,47 @@ const ThirdPartyProfile = () => {
       <br />
       <form>
         <StextField
+          label="Email"
+          name="email"
+          value={inputs.email || email}
+          onChange={handleChange}
+        />
+        <StextField
+          label="Mobile Number"
+          name="mobile"
+          value={inputs.mobile || mobile}
+          onChange={handleChange}
+        />
+
+        <StextField
           label="Address"
           name="address"
-          // value={inputs.address || ""}
-          // onChange={handleChange}
+          value={inputs.address || address}
+          onChange={handleChange}
         />
+        <FormControl sx={{ width: "70ch" }}>
+          <InputLabel id="verificationDocumentType">
+            Verification Document Type
+          </InputLabel>
+          <Select
+            labelId="verificationDocumentType"
+            name="verifyDocType"
+            value={inputs.verifyDocType || verifyDocType}
+            label="Verification document type"
+            onChange={handleChange}
+          >
+            <MenuItem value="Degree Certificate">Degree Certificate</MenuItem>
+            <MenuItem value="O/L, A/L Certificate">
+              O/L, A/L Certificate
+            </MenuItem>
+            <MenuItem value="NVQ Level Certificate">
+              NVQ Level Certificate
+            </MenuItem>
+            <MenuItem value="Affidavit">Affidavit</MenuItem>
+          </Select>
+        </FormControl>
+        <br />
+        <br />
         <Sbutton text="Update" type="submit" onClick={onSubmit} />
       </form>
     </div>
