4abe3180b0fc0ebe53357ce5b4fb0b90eac286e4
kathu@08 <sivalingamk.19@uom.lk>
2022-03-11 11:27:32 +0530

    added cards in admin dashboard

--- client/src/components/BasicCard.js
+++ client/src/components/BasicCard.js
@@ -1,28 +1,24 @@
-import * as React from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
-const card = (
-  <React.Fragment>
-    <CardContent>
-      <Typography variant="h5" gutterBottom>
-        Total Service Providers
-      </Typography>
-      <Typography variant="h3" textAlign="center">
-        10
-      </Typography>
-    </CardContent>
-  </React.Fragment>
-);
-
-export default function OutlinedCard() {
+const BasicCard = ({ text, count }) => {
   return (
     <Box>
-      <Card variant="outlined" sx={{ minHeight: 150 }}>
-        {card}
+      <Card variant="outlined" sx={{ minHeight: 150, minWidth: 300 }}>
+        <CardContent>
+          <Typography variant="h5" textAlign="center">
+            {text}
+          </Typography>
+          <br />
+          <Typography variant="h3" textAlign="center">
+            {count}
+          </Typography>
+        </CardContent>
       </Card>
     </Box>
   );
-}
+};
+
+export default BasicCard;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,26 @@
 import BasicCard from "../components/BasicCard";
 import { Grid } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  gridContainer: {
+    paddingLeft: "20px",
+    paddingRight: "20px",
+  },
+});
 
 const Dashboard = () => {
+  const classes = useStyles();
   return (
-    <Grid container spacing={4}>
-      <Grid item>
-        <BasicCard />
+    <Grid container spacing={4} className={classes.gridContainer}>
+      <Grid item xs={4}>
+        <BasicCard text="Total Service Providers" count="10" />
+      </Grid>
+      <Grid item xs={4}>
+        <BasicCard text="Total Service Consumers" count="12" />
+      </Grid>
+      <Grid item xs={4}>
+        <BasicCard text="Completed Jobs" count="12" />
       </Grid>
     </Grid>
   );
