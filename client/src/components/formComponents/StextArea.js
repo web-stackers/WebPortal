05ddd57d7262add1222d2aa61ec97ee15ec29d04
Vellummyilum Vinoth <86709705+Windyvinoth13@users.noreply.github.com>
2022-03-15 16:03:34 +0530

    Update StextArea.js

--- client/src/components/formComponents/StextArea.js
+++ client/src/components/formComponents/StextArea.js
@@ -1,44 +1,15 @@
-import TextareaAutosize from "@mui/material/TextareaAutosize";
-import Grid from "@material-ui/core/Grid";
-import TextField from "@mui/material/TextField";
-import { createMuiTheme,MuiThemeProvider } from "@material-ui/core";
-import * as React from "react";
+import * as React from 'react';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
 
-const theme = createMuiTheme({
-  props: {
-    MuiTextField: {
-      variant: "outlined"
-      
-    }
-  }
-});
-
-const StextArea = () => {
-  const [text, setText] = React.useState("");
-
-  const handleChange = (event) => {
-    setText(event.target.value);
-  };
-
+const StextArea=()=> {
   return (
-    <MuiThemeProvider theme={theme}>
-    <Grid container direction="row" spacing={1}>
-      <Grid item sm={6}>
-        <TextField
-          sx={{ width: "70ch" }}
-          multiline
-          label="Response"
-          InputProps={{
-            inputComponent: TextareaAutosize,
-            rows: 3
-          }}
-          value={text}
-          onChange={handleChange}
-        />
-      </Grid>
-    </Grid>
-  </MuiThemeProvider>
+    <TextareaAutosize
+      aria-label="minimum height"
+      minRows={3}
+      placeholder="Response"
+      style={{ width: 550 }}
+    />
   );
-};
+}
 
 export default StextArea;
