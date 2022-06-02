28b4332a7ce390d2d5e8f10ec6129c2572ddbdb8
Vellummyilum Vinoth <86709705+Windyvinoth13@users.noreply.github.com>
2022-06-03 00:40:58 +0530

    Update ResponseToComplaint.js

--- client/src/pages/complaints/ResponseToComplaint.js
+++ client/src/pages/complaints/ResponseToComplaint.js
@@ -1,99 +1,90 @@
-import { useLocation } from 'react-router-dom'
-import dateFormat from 'dateformat'
-import Typography from '@mui/material/Typography'
-import StextField from '../../components/formComponents/StextField'
-import StextArea from '../../components/formComponents/StextArea'
-import Sbutton from '../../components/Sbutton'
-import Job from '../../services/Job'
-import { useState } from 'react'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import dateFormat from "dateformat";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import CardActions from "@mui/material/CardActions";
+import StextField from "../../components/formComponents/StextField";
+import Sbutton from "../../components/Sbutton";
+
+import Job from "../../services/Job";
 
 const ResponseToComplaint = () => {
-  const location = useLocation()
-  const ID = location.state._id
-  const whom = location.state.by
-  const category = location.state.category
-  const date = location.state.date
-  const description = location.state.description
+  const location = useLocation();
+  const ID = location.state.id;
+  const whom = location.state.by;
+  const category = location.state.category;
+  const date = location.state.date;
+  const description = location.state.description;
 
-  const [reply, setReply] = useState({})
+  const [reply, setReply] = useState({});
 
-  const handleChange = event => {
-    const name = event.target.name
-    const value = event.target.value
-    setReply(values => ({ ...values, [name]: value }))
-  }
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setReply((values) => ({ ...values, [name]: value }));
+  };
 
   //It is given as onClick function in submit button to redirect to the main page
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const routeChange = () => {
-    let path = '/complaint'
-    navigate(path)
-  }
+    let path = "/complaints";
+    navigate(path);
+  };
 
   //onClick function when submit button is clicked. Details will be update and path will be redirected
-  const onSubmit = e => {
-    e.preventDefault()
-    console.log(reply)
-    Job.updateThirdPartyByID(ID, reply)
-    routeChange()
-  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    Job.complaintHandled(ID, reply);
+    console.log(ID);
+    console.log(reply);
+    routeChange();
+  };
 
   return (
     <div>
-      <Typography variant='h4'>{whom}</Typography>
+      <Typography variant="h7">Complaint Date </Typography>
+      <Typography variant="h6"> {dateFormat(date, "yyyy-mm-dd")}</Typography>
+      <br />
+      <Typography variant="h7">By Whom </Typography>
+      <Typography variant="h6"> {whom}</Typography>
+      <br />
+      <Typography variant="h7">Category </Typography>
+      <Typography variant="h6"> {category}</Typography>
+      <br />
+      <Typography variant="h7">Description </Typography>
+      <Typography variant="h6"> {description}</Typography>
       <br />
       <form>
         <div>
           <StextField
-            label='Date'
-            name='date'
-             value={reply.date || dateFormat(date, "yyyy-mm-dd")}
-            onChange={handleChange}
-          />
-
-          <StextField
-            label='By whom'
-            name='byWhom'
-            value={reply.whom || whom}
-            onChange={handleChange}
-          />
-
-          <StextField
-            label='Category'
-            name='category'
-            value={reply.category || category}
-            onChange={handleChange}
-          />
-
-          <StextField
-            label='Description'
-            name='description'
-            value={reply.description || description}
-            onChange={handleChange}
-          />
-          <StextArea
-            label='Admin Response'
-            name='adminResponse'
-            value={reply.adminResponse || ''}
+            label="Admin Response"
+            placeholder="Admin Response"
+            name="adminResponse"
+            value={reply.adminResponse || " "}
             onChange={handleChange}
           />
         </div>
-        <Sbutton
-          text='Send'
-          type='submit'
-          onClick={onSubmit}
-          btnWidth='15%'
-          marginRight='5%'
-        />
+        <CardActions>
+          <Sbutton
+            text="Send"
+            type="submit"
+            onClick={onSubmit}
+            btnWidth="150px"
+            marginRight="5%"
+          />
+          <Link
+            to="/complaints"
+            className="link"
+            style={{ marginRight: "50%" }}
+          >
+            <Sbutton text="Back" btnWidth="150px" marginRight="5%" />
+          </Link>
+        </CardActions>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default ResponseToComplaint
+export default ResponseToComplaint;
