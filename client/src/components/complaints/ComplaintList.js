d74c4c2eb044d3b6adc4300ea3f9070309132f3f
Vellummyilum Vinoth <86709705+Windyvinoth13@users.noreply.github.com>
2022-06-03 00:38:56 +0530

    Update ComplaintList.js

--- client/src/components/complaints/ComplaintList.js
+++ client/src/components/complaints/ComplaintList.js
@@ -1,102 +1,62 @@
-import { useEffect, useState } from "react";
-import dateFormat from 'dateformat';
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import Job from "../../services/Job";
-import Sbutton from "../Sbutton";
-import { makeStyles } from "@mui/styles";
+import { useEffect } from "react";
+import dateFormat from "dateformat";
 import { Link } from "react-router-dom";
+import { DataGrid } from "@mui/x-data-grid";
+import Sbutton from "../Sbutton";
+import CardActions from "@mui/material/CardActions";
 
-const useStyles = makeStyles((theme) => ({
-  //class name for card
-  //The flex layout allows responsive elements within a container to be automatically arranged depending upon screen size. Flex container becomes flexible by setting the display property to flex
-  root: {
-    display: "flex",
-    height: "200px",
-    backgroundColor: "transparent !important",
-    color: "white",
-  },
-  //class name for div where card content tag is present
-  details: {
-    display: "flex",
-    flexDirection: "column",
-    padding: 10,
-    width: "50%",
-  },
-  //class name for card media
-  cover: {
-    width: "20%",
-  },
-}));
-
-const ComplaintList = () => {
-  const [listOfComplaints, setListOfComplaints] = useState([]);
-
-  //Get all secondary users
-  const fetchAllComplaints = () => {
-    Job.fetchComplaints()
-      .then((response) => {
-        setListOfComplaints(response.data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
+const ComplaintList = ({ type, listOfComplaints, fetchAllComplaints }) => {
 
   //By using this Hook, you tell React that your component needs to do something after render. Call it later after performing the DOM updates.
   useEffect(() => {
     fetchAllComplaints();
-  }, []);
+  }, [type]);
 
-  const classes = useStyles();
+  const rows = listOfComplaints.map((item) => {
+    return {
+      id: item.complaint[0]._id,
+      by: item.complaint[0].by,
+      date: dateFormat(item.complaint[0].date, "yyyy-mm-dd"),
+      category: item.complaint[0].category,
+      description: item.complaint[0].description,
+      adminResponse: item.complaint[0].adminResponse,
+    };
+  });
+
+  const columns = [
+    { field: "date", headerName: "Date", width: 150 },
+    { field: "category", headerName: "Category", width: 250 },
+    { field: "description", headerName: "Description", width: 400 },
+    { field: "adminResponse", headerName: "Admin Response", width: 200 },
+    {
+      field: "Action",
+      headerName: "Action",
+      width: 220,
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <CardActions>
+            <Link
+              to="/responseToComplaint"
+              state={params.row}
+              className="link"
+              style={{ marginRight: "50%" }}
+            >
+              <Sbutton text="Response" btnWidth="90%" />
+            </Link>
+          </CardActions>
+        );
+      },
+    },
+  ];
 
   return (
-    <div>
-      {listOfComplaints.map((jobs) => {
-        return (
-          <div>
-            {jobs.complaint.map((complaint) => {
-              return (
-                <div key={complaint._id}>
-                  <Card className={classes.root}>
-                    <div className={classes.details}>
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                          {complaint.by}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          <b>Complaint Date :</b> {dateFormat(complaint.date, "yyyy-mm-dd")}
-                          <br />
-                          <b>Complaint category:</b> {complaint.category}
-                          <br/>
-                          <b>Description : </b>
-                          {complaint.description}
-                          <br />
-                          <b>Admin Response :</b> {complaint.adminResponse}
-                        </Typography>
-                      </CardContent>
-                    </div>
-
-                    <CardActions>
-                      <Link
-                        to="/responseToComplaint"
-                        state={complaint}
-                        className="link"
-                        style={{ marginRight: "50%" }}
-                      >
-                        <Sbutton text="Response" btnWidth="90%" />
-                      </Link>
-
-                    </CardActions>
-                  </Card>
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
+    <div style={{ height: 500, width: "100%" }}>
+      <div style={{ display: "flex", height: "100%" }}>
+        <div style={{ flexGrow: 1 }}>
+          <DataGrid rows={rows} columns={columns} disableSelectionOnClick />
+        </div>
+      </div>
     </div>
   );
 };
