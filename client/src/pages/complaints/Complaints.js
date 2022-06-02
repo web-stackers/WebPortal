9005cb1b2002e8b890b5cb89294647a9adb97ab5
Vellummyilum Vinoth <86709705+Windyvinoth13@users.noreply.github.com>
2022-06-03 00:40:26 +0530

    Update Complaints.js

--- client/src/pages/complaints/Complaints.js
+++ client/src/pages/complaints/Complaints.js
@@ -1,14 +1,49 @@
+import React from "react";
+import { useState } from "react";
+
+import Job from "../../services/Job";
+import Topbar from "../../components/Users/Topbar";
 import ComplaintList from "../../components/complaints/ComplaintList";
 
 const Complaint = () => {
+  const [user, setUser] = useState("Consumers");
+  const [listOfComplaints, setListOfComplaints] = useState([]);
+
+  const fetchAllComplaints = () => {
+    if (user == "Consumers") {
+      Job.fetchComplaintsByConsumer()
+        .then((response) => {
+          setListOfComplaints(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    } else {
+      Job.fetchComplaintsByProvider()
+        .then((response) => {
+          setListOfComplaints(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <br /> <br />
-        {/* Retrieving card structure of third party created in complaints list */}
-        <ComplaintList />
-      </div>
-    </div>
+    <>
+      <Topbar
+        type={user}
+        setType={setUser}
+        setListOfComplaints={setListOfComplaints}
+        fetchAllComplaints={fetchAllComplaints}
+      />
+      <ComplaintList
+        type={user}
+        listOfComplaints={listOfComplaints}
+        setListOfComplaints={setListOfComplaints}
+        fetchAllComplaints={fetchAllComplaints}
+      />
+    </>
   );
 };
 
