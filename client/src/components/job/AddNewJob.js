da7efe80ad552197b99f92cf5e37c8c9db6dc987
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2022-06-04 18:35:09 +0530

    small edit in previous commit

--- client/src/components/job/AddNewJob.js
+++ client/src/components/job/AddNewJob.js
@@ -10,11 +10,25 @@ const AddNewJob = ({ onAdd }) => {
   const [jobType, setJobType] = useState("");
   const [category, setCategory] = useState("");
   const [description, setDescription] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    let temp = {};
+    temp.jobType = jobType ? "" : "This field is required.";
+
+    setErrors({
+      ...temp,
+    });
+    return Object.values(temp).every((x) => x === ""); //every() method tests whether all elements in the array pass the test implemented by the provided function. It retruns a boolean value
+  };
+
   // When form is submitted, onSubmit function will be performed, where onAdd function will be called where we post new document in jobTypeCategory collection. And after than page will be autorefreshed
   const onSubmit = (e) => {
     e.preventDefault();
-    onAdd({ jobType, category, description });
-    window.location.reload(false);
+    if (validate()) {
+      onAdd({ jobType, category, description });
+      window.location.reload(false);
+    }
   };
   return (
     <form>
@@ -24,6 +38,7 @@ const AddNewJob = ({ onAdd }) => {
         name="jobType"
         value={jobType}
         onChange={(e) => setJobType(e.target.value)}
+        error={errors.jobType}
       />
       <FormControl sx={{ width: "70ch" }}>
         <InputLabel id="category">Category</InputLabel>
