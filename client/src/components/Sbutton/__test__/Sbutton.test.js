9d986955e09d9d22da51a61c3dd75f5c420739c6
Pavadaran <pavadaran17@gmail.com>
2022-06-05 22:48:29 +0530

    Unit test for Sbutton component

--- client/src/components/Sbutton/__test__/Sbutton.test.js
+++ client/src/components/Sbutton/__test__/Sbutton.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import Sbutton from '../Sbutton';
+import Sbutton from '../index';
 
 test('renders passes paramter in Sbutton component', () => {
     render(<Sbutton text="Click" />);
