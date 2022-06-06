130ae2fb1178277e8df1e14c5e1a70331886af13
Pavadaran <pavadaran17@gmail.com>
2022-06-06 15:13:43 +0530

    Unit test for Alertbox

--- client/src/components/Sbutton/__test__/Sbutton.test.js
+++ client/src/components/Sbutton/__test__/Sbutton.test.js
@@ -3,7 +3,7 @@ import "@testing-library/jest-dom";
 
 import Sbutton from "../index";
 
-test("renders passes paramter in Sbutton component", () => {
+test("renders passed parameter in Sbutton component", () => {
   render(<Sbutton text="Click" />);
   const buttonElement = screen.getByText(/click/i);
   expect(buttonElement).toBeInTheDocument();
