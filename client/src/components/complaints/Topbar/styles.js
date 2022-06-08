390bfc32ec2bb7594571635fa0422ac2d481680e
Windyvinoth13 <vinothv.19@uom.lk>
2022-06-08 22:03:17 +0530

    update Topbar

---
+++ client/src/components/complaints/Topbar/styles.js
@@ -0,0 +1,22 @@
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => {
+    return {
+        outline: {
+            display: "flex",
+            justifyContent: "space-between",
+            fontSize: 18,
+            height: "fit-content",
+            margin: "-5px 0 10px 0",
+            borderBottom: "2px " + theme.palette.primary.light + " solid",
+            padding: "0px 10px 8px 0",
+          },
+          search: {
+            display: "flex",
+            justifySelf: "flex-end",
+            alignContent: "space-between",
+          },
+    }
+});
+
+export default useStyles;
